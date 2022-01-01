8ebc1eff1107aec906402593be218fe78c63a6ad
ilsun2 <71248667+ilsun2@users.noreply.github.com>
2022-01-01 18:33:56 +0900

    #~3-4

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+
+    GANDER_MALE = "Male"
+    GANDER_FEMAEL = "Female"
+    GANDER_OTHER = "Other"
+
+    GANDER_CHOICE = (
+         (GANDER_MALE , "Male"),
+    (GANDER_FEMAEL , "Female"),
+    (GANDER_OTHER , "Other")
+    )
+
+    bio = models.TextField(default="",null=True,blank=True)
+    avatar = models.ImageField(null = True,blank = True)
+    birthday = models.DateField(null = True,blank = True)
+    gander = models.CharField(choices=GANDER_CHOICE,max_length=10,null = True,blank = True)
+    
